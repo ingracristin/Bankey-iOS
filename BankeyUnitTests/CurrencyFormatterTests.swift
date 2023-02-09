b0d0fe77febaf6500f1fad60f374a6811a72f0bd
ingra.cristin <ingracristinaom@gmail.com>
2023-02-09 13:59:12 -0400

    unit test error

--- BankeyUnitTests/CurrencyFormatterTests.swift
+++ BankeyUnitTests/CurrencyFormatterTests.swift
@@ -21,8 +21,32 @@ class CurrencyFormatterTests: XCTestCase {
     
     func testBreakDollarsIntoCents() throws {
         let result = formatter.breakIntoDollarsAndCents(929466.23)
-        //XCTAssertEqual(result.0, "929,466") ----> Não está indo
+        //XCTAssertEqual(result.0, "929,466")
         XCTAssertNotEqual(result.0, "929,466")
         XCTAssertEqual(result.1, "23")
     }
+/*
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "$929,466.23")
+    }
+    
+    // Challenge: You write
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0.00)
+        XCTAssertEqual(result, "$0.00")
+    }
+    
+    func testDollarsFormattedWithCurrencySymbol() throws {
+        let locale = Locale.current
+        let currencySymbol = locale.currencySymbol!
+        
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "\(currencySymbol)929,466.23s")
+    }
+ 
+ */
+    
+    //Precisa passar o teste pra location certa
+    
 }
